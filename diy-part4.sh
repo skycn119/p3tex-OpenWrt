c388a672a375e57eebe397403940598f630f509c
skycn119 <skycn0743@hotmail.com>
2026-05-02 20:22:28 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -16,5 +16,4 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git eqosplus  https://github.com/skycn119/eqosplus' >> feeds.conf.default
-echo 'src-git nftqos  https://github.com/ilxp/openwrt-nft-qos' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
